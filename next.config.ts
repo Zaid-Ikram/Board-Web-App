5909fdd1db1572532e56557741aa29f8878b448e
Zaid-Ikram <zaidikram2710@gmail.com>
2025-02-25 18:14:19 +0500

    Day 2 of Making Board Web App

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,21 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "img.clerk.com"
+      }
+    ]
+  }
 };
 
 export default nextConfig;
+// https://www.eporner.com/video-1B7vofVNdUk/secret-adultery-with-colleague-english-subtitle/
+// https://www.eporner.com/video-0uIULQu3604/hot-spring-gangbang-blond-married-woman-from-the-west-gets-fucked-at-a-coed-hot-spring/
+// https://www.eporner.com/video-w6DOZkxi1Rc/m-yuk1-1to-cant-control-her-unprotected-plump-buttocks/
+
+// https://missav123.com/en/ssis-202-english-subtitle
+// https://missav123.com/dm2/en/jul-990-english-subtitle
+// https://sextb.net/ssis-202-rm
